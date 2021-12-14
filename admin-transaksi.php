8f59d48812cb4a0e870403ec1e88d289fd60caa7
Raihan Alifianto <purplelamborghini24@gmail.com>
2021-12-14 15:05:23 +0700

    fix sidebar

--- admin-transaksi.php
+++ admin-transaksi.php
@@ -43,81 +43,8 @@
 <body id="page-top">
 
     <div id="wrapper">
-
-            <!-- Sidebar -->
-            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/utama">
-        <div class="sidebar-brand-icon">
-            <img src="{{ asset('img/logo.jpg') }}" width="50" height="50">
-        </div>
-        <div class="sidebar-brand-text mx-3">Hantarin</div>
-    </a>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="/utama">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Data
-    </div>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="/transaksi">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Riwayat Transaksi</span></a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/tambah">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Tambah Produk</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Account
-    </div>
-
-    <!-- Nav Item - Profile -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user"></i>
-            <span>Profile</span></a>
-    </li>  
-
-
-    <!-- Nav Item - Logout -->
-    <li class="nav-item">
-        <a class="nav-link" href="/">
-            <i class="fas fa-sign-out-alt"></i>
-            <span>Log Out</span></a>
-    </li>   
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-    </ul>
+    <!-- Sidebar -->
+    <?php include ('./fragment/sidebar.php'); ?>
     <!-- End of Sidebar -->
 
     <div id="content-wrapper" class="d-flex flex-column">
@@ -672,25 +599,6 @@
     <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
-            </div>
-        </div>
-    </div>
-    </div>
     <!-- Bootstrap core JavaScript-->
     <script src="admin/vendor/jquery/jquery.min.js"></script>
     <script src="admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
---
+++ fragment/sidebar.php
@@ -0,0 +1,73 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/utama">
+                <div class="sidebar-brand-icon">
+                    <img src="./img/logo.jpg" width="50" height="50">
+                </div>
+                <div class="sidebar-brand-text mx-3">Hantarin</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="./admin-utama.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Data
+            </div>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="./admin-transaksi.php">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Riwayat Transaksi</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./admin-tambah.php">
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Tambah Produk</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Account
+            </div>
+
+            <!-- Nav Item - Profile -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-fw fa-user"></i>
+                    <span>Profile</span></a>
+            </li>  
+
+            
+            <!-- Nav Item - Logout -->
+            <li class="nav-item">
+                <a class="nav-link" type="button" href="./php/check_logout.php">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Log Out</span></a>
+            </li>   
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
